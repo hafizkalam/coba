d1f9022ee316bf53eb89565593353c976b8e48d3
hafizkalam <hafizkalamabdillah@gmail.com>
2023-06-29 15:00:57 +0700

    edit

---
+++ resources/views/layout/cart.blade.php
@@ -0,0 +1,149 @@
+@extends('layout.viewmenu')
+
+@section('content')
+    <section id="contact" class="contact">
+        <div class="container " data-aos="fade-up">
+            <div class="p-3 p-md-4">
+                {{-- PESAN --}}
+                @if (session('pesan'))
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        <strong><i class="bi bi-check2-all"></i>&nbsp;{{ session('pesan') }}.</strong>
+                    </div>
+                @endif
+            </div>
+            <div class="tab-content" data-aos="fade-up" data-aos-delay="300">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="{{ route('menu.index') }}">Menu</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Cart</li>
+                    </ol>
+                </nav>
+                <div class="tab-pane fade active show" id="menu-starters">
+                    <div class="tab-header text-center">
+                        <h3><strong>Keranjang Pesanan</strong></h3>
+                        <br><br>
+                    </div>
+                </div>
+            </div>
+            <table class="table table-hover" id="cart">
+                <thead>
+                    <th scope="col">Nama Pesanan</th>
+                    <th scope="col">Harga</th>
+                    <th scope="col">Jumlah</th>
+                    <th scope="col">Total Harga</th>
+                    <th scope="col">Hapus</th>
+                </thead>
+                <tbody>
+                    @php $total = 0 @endphp
+                    @if (session('cart'))
+                        @foreach (session('cart') as $id_menu => $details)
+                            @php $total += $details['harga'] * $details['quantity'] @endphp
+                            <tr data-id="{{ $id_menu }}">
+                                <td>
+                                    {{ $details['nama_makanan'] }}
+                                </td>
+                                <td>Rp.{{ $details['harga'] }}</td>
+                                <td data-th="Quantity">
+                                    <input type="number" value="{{ $details['quantity'] }}"
+                                        class="form-control quantity update-cart" />
+                                </td>
+                                <td class="text-center">Rp.{{ $details['harga'] * $details['quantity'] }}</td>
+                                <td class="actions">
+                                    <button class="btn btn-danger btn-sm remove-from-cart"><i
+                                            class="bi bi-trash-fill"></i></button>
+                                </td>
+                            </tr>
+                        @endforeach
+                    @endif
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colspan="5" class="text-right">
+                            <h3><strong>Total Rp.{{ $total }}</strong></h3>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="5" class="text-right">
+                            <a href="{{ route('menu.index') }}" class="btn btn-warning"><i
+                                    class="bi bi-bag-plus-fill"></i>Tambah Pesanan</a>
+                            @if (session('cart') >= 1)
+                                <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                                    data-bs-target="#exampleModal">
+                                    Checkout
+                                </button>
+                            @else
+                            @endif
+                            {{-- <a href="{{ route('deleteCart') }}">delete</a> --}}
+                        </td>
+                    </tr>
+                </tfoot>
+            </table>
+        </div>
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Pilih Metode Pemabayaran</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <center>
+                            Silahkan pilih salah satu metode pembayaran
+                        </center>
+                    </div>
+                    <div class="modal-footer">
+                        {{-- <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" style="float: left">Close</button> --}}
+                        <div class="btn-group">
+                            <a href="{{ route('transaksiTunai') }}" type="button" class="btn btn-warning"><i
+                                    class="bi bi-cash"></i>&nbsp;Tunai</a>
+                            <a href="{{ route('transaksi') }}" type="button" class="btn btn-success"><i
+                                    class="bi bi-credit-card-2-back-fill"></i>&nbsp;Non-Tunai</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script type="text/javascript">
+        $(".update-cart").change(function(e) {
+            e.preventDefault();
+
+            var ele = $(this);
+
+            $.ajax({
+                url: '{{ route('update.cart') }}',
+                method: "patch",
+                data: {
+                    _token: '{{ csrf_token() }}',
+                    id_menu: ele.parents("tr").attr("data-id"),
+                    quantity: ele.parents("tr").find(".quantity").val()
+                },
+                success: function(response) {
+                    window.location.reload();
+                }
+            });
+        });
+
+        $(".remove-from-cart").click(function(e) {
+            e.preventDefault();
+
+            var ele = $(this);
+
+            if (confirm("Apakah anda yakin menghapus pesanan ini?")) {
+                $.ajax({
+                    url: '{{ route('remove.from.cart') }}',
+                    method: "DELETE",
+                    data: {
+                        _token: '{{ csrf_token() }}',
+                        id_menu: ele.parents("tr").attr("data-id")
+                    },
+                    success: function(response) {
+                        window.location.reload();
+                    }
+                });
+            }
+        });
+    </script>
+@endsection
--- resources/views/layout/viewmenu.blade.php
+++ resources/views/layout/viewmenu.blade.php
@@ -49,22 +49,14 @@
                 <img src="assets/img/logo.png" alt="">
                 {{-- <h1>{{ $judulheader }}<span>.</span></h1> --}}
             </a>
-            <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-                <!-- Right navbar links -->
-                <ul class="navbar-nav ml-auto">
-                    <!-- Messages Dropdown Menu -->
-                    <li class="nav-item dropdown">
-                        <a class="nav-link" data-toggle="dropdown" href="#">
-                            <i class="far fa-shopping-cart"></i>
-                        </a>
-                    </li>
-                </ul>
 
-                <button class="btn bg-dark rounded-pill text-white ">
-                    Nomor Meja {{ request()->id }}
-                </button>
+            <a class="nav" href="cart">
+                <i class="fas fa-shopping-cart fa-2" style="color: black"></i>
+            </a>
 
-            </nav>
+            <button class="btn bg-dark rounded-pill text-white ">
+                Nomor Meja {{ request()->id }}
+            </button>
 
         </div>
     </header><!-- End Header -->
@@ -103,7 +95,7 @@
 
                             <div class="tab-header text-center">
                                 <p>Menu</p>
-                                <h3>{{ $key }}</h3>
+                                {{-- <h3>{{ $key }}</h3> --}}
                             </div>
 
                             <div class="row gy-5">
@@ -135,38 +127,15 @@
     <!-- ======= Footer ======= -->
     <footer id="footer" class="footer">
         <div class="container">
-            <div class="row gy-3">
-                <div class="col-lg-3 col-md-6 d-flex">
-                    <i class="bi bi-geo-alt icon"></i>
-                    {{-- <div>
-             <h4>{{ $judulalamat }}</h4>
-             <p>
-                 {{ $alamat }}<br>
-             </p>
-           </div> --}}
-                </div>
-                <div class="col-lg-3 col-md-6 footer-links d-flex">
-                    <i class="bi bi-clock icon"></i>
-                    <div>
-                        {{-- <h4>{{ $juduljamoperasional }}</h4>
-             <p>
-               <strong>{{ $jamoperasional }}</strong> --}}
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="container">
             <div class="copyright">
-                &copy; Copyright <strong><span>Glory Tech.</span></strong>. All Rights Reserved
+                &copy; Copyright <strong><span>Glory Tech.</span></strong> All Rights Reserved
             </div>
             <div class="credits">
                 <!-- All the links in the footer should remain intact. -->
                 <!-- You can delete the links only if you purchased the pro version. -->
                 <!-- Licensing information: https://bootstrapmade.com/license/ -->
                 <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/yummy-bootstrap-restaurant-website-template/ -->
-                Designed by <a href="https://bootstrapmade.com/">Glory Tech.</a>
+                Designed by <strong>Glory Tech.</strong>
             </div>
         </div>
 
